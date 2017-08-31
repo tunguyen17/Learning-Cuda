e0270dffe6f1260ac23f430814e248d0a2e3a260
Tu Nguyen <tu.nguyen@go.tarleton.edu>
2017-08-31 18:40:06 -0500

    some changes

--- hello.c
+++ hello.c
@@ -1,6 +1,30 @@
-#include "book/common/book.h"
+#include <iostream> 
 
-init main( void ){
-	printf("Hello, World!\n");
-	return 0;
+__global__ void add( int a, int b, int *c){
+	# global qualifier to standard C
+	# Alerts the compiler that a function should be compiled to run on a device instead of the host
+	*c = a + b;
 }
+
+int main( void ) {
+	int c;
+	int *dev_c;
+	
+	# cudaMalloc allocate memory on the device
+		# first argument: pointer to pointer of the address of the value you want to make
+		# second argument: size of the allocation 	
+
+	HANDLE_ERROR( cudaMalloc( (void**)&dev_c, sizeof(int) ) );
+	
+	add<<1,1>>(2, 7, dev_c);
+	
+	HANDLE_ERROR( cudaMemcpy( &c,
+				  dev_c,
+				  sizeof(int),
+				  cudaMemcpyDeviceToHost ) );
+
+	printf( "2 + 7 = %d\n", c );
+	cudaFree(dev_c);
+
+	return 0;
+}	
